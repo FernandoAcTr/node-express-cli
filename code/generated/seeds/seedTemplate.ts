e06f9ffb0d125f85a886c7748c278dd6175cc660
Fernando Acosta <fernandodeveloper99@gmail.com>
2023-05-06 16:58:54 -0600

    Update seed template

--- code/generated/seeds/seedTemplate.ts
+++ code/generated/seeds/seedTemplate.ts
@@ -1,6 +1,7 @@
 import { Seed } from '../seed'
+
 export class __ClassName__ implements Seed {
-  name: string = ''
+  name: string = '__ClassName__'
 
   seed(): Promise<void> {
     throw new Error('Method not implemented.')
