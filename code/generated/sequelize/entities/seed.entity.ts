897d79ddb10f2d49ce633d83d5132f50ed672670
Fernando Acosta <lacosta@gobdigital.com>
2023-08-01 13:37:23 -0600

    Fix sequelize seed bug

--- code/generated/sequelize/entities/seed.entity.ts
+++ code/generated/sequelize/entities/seed.entity.ts
@@ -8,7 +8,7 @@ import {
 } from "sequelize";
 
 export class Seed extends Model<InferAttributes<Seed>, InferCreationAttributes<Seed>> {
-  declare id: CreationOptional<number>;
+  declare id: CreationOptional<string>;
   declare createdAt: CreationOptional<Date>;
 }
 
